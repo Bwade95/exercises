3284287c5721810afd397b6b04adf2ac18ec714b
Brandon <bwade.95@hotmail.com>
2021-07-08 13:47:10 +0100

    refactored valid time check

--- functions.erl
+++ functions.erl
@@ -1,6 +1,5 @@
 -module(functions).
 -compile(export_all). %% replace with -export(). later
--define(is_format(F), tuple_size(F) =:= 3 andalso is_integer(element(1, F)) andalso is_integer(element(2, F)) andalso is_integer(element(3, F))).
 
 head([H|_]) -> H.
 second([_,X|_]) -> X.
@@ -10,8 +9,11 @@ same(X,X) ->
 same(_,_) ->
     false.
 
-valid_time({Date = {Y,M,D}, Time = {H,Min,S}}) when ?is_format(Date), ?is_format(Time) ->
-    io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n", [Date,Y,M,D]),
-    io:format("The Time tuple (~p) indicates: ~p:~p:~p.~n", [Time,H,Min,S]);
+valid_time({Date = {Y,M,D}, Time = {H,Min,S}}) ->
+    case is_valid(Date) andalso is_valid(Time) of
+        true -> io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n", [Date,Y,M,D]),
+                io:format("The Time tuple (~p) indicates: ~p:~p:~p.~n", [Time,H,Min,S])  
+    end;  
 valid_time(_) ->
     io:format("Stop feeding me wrong data!~n").
+is_valid(F) when is_integer(element(1, F)) andalso is_integer(element(2, F)) andalso is_integer(element(3, F)) -> true.
